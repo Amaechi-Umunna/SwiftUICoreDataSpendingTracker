f9f9a4960cedea6df676e44c0d132c9ec9d8cd73
Amaechi-Umanna <100558243+Umunnabuike@users.noreply.github.com>
2022-12-08 07:40:07 +0100

    Initial Commit

---
+++ SwiftUICoreDataSpendingTracker/SwiftUICoreDataSpendingTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUICoreDataSpendingTrackerApp.swift
+//  SwiftUICoreDataSpendingTracker
+//
+//  Created by Amaechi Chukwu on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoreDataSpendingTrackerApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
